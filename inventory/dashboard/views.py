3020a08bf23207f69bbe1ce555e5e58b1445b2aa
RamadanYosiPratama <ramadanyosi30@gmail.com>
2021-10-23 02:18:37 +0700

    Memasang Template di Django

--- inventory/dashboard/views.py
+++ inventory/dashboard/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Halaman Django Pertama Rama')
+    return render(request, 'dashboard/index.html')
+def staff(request):
+    return render(request, 'staff.html')
